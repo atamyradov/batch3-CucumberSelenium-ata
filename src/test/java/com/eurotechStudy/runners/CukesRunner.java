437382e71f443ac75e6d328fde3013b298bce539
atamyradov <atamyradov2018@gmail.com>
2022-06-01 21:08:09 +0200

    5 class ayni anda güncellendi

--- src/test/java/com/eurotechStudy/runners/CukesRunner.java
+++ src/test/java/com/eurotechStudy/runners/CukesRunner.java
@@ -9,19 +9,24 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"json:target/cucumber.json",
                 "html:target/default-html-reports",
-                "de.monochromata.cucumber.report.PrettyReports:target/cucumber"
+                "de.monochromata.cucumber.report.PrettyReports:target/cucumber",
+                "rerun:target/rerun.txt"
         },
         features = "src/test/resources/features",
         glue = "com/eurotechStudy/step_definitions",
         dryRun = false,
-        tags = "@wip"
-        //"@navigate"
-        //"@type"
-        //"@account"
-
-        // Before version 4  {"@wip", "@smoke"} - - and
-        // Before version 5  "@wip, @smoke" - - or
-        // Before version 5    {"@smoke" , "~wip"} - - and not
+        tags = "@edit"
+                //"@FailedTest"
 )
 public class CukesRunner {
 }
+
+
+//"@edit"
+//"@navigate"
+//"@type"
+//"@account"
+
+// Before version 4  {"@wip", "@smoke"} - - and
+// Before version 5  "@wip, @smoke" - - or
+// Before version 5    {"@smoke" , "~wip"} - - and not
